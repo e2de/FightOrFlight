c8bf984b7c1576fad441e83e6c8671b0b090562d
e2de <mng.alice@gmail.com>
2019-03-04 23:23:30 -0800

    rename and move First Person Character cpp

--- Source/FightOrFlight/ChooseNextWaypoint.cpp
+++ Source/FightOrFlight/ChooseNextWaypoint.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Public/ChooseNextWaypoint.h"
+#include "ChooseNextWaypoint.h"
 #include "Classes/BehaviorTree/BehaviorTreeTypes.h"
 #include "Classes/BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
--- Source/FightOrFlight/Player/FirstPersonCharacter.h
+++ Source/FightOrFlight/Player/FirstPersonCharacter.h
@@ -4,12 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "FightOrFlightCharacter.generated.h"
+#include "FirstPersonCharacter.generated.h"
 
 class UInputComponent;
 
 UCLASS(config=Game)
-class AFightOrFlightCharacter : public ACharacter
+class AFirstPersonCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
@@ -46,7 +46,7 @@ class AFightOrFlightCharacter : public ACharacter
 	class UMotionControllerComponent* L_MotionController;
 
 public:
-	AFightOrFlightCharacter();
+	AFirstPersonCharacter();
 
 protected:
 	virtual void BeginPlay();
